c8520623a15a3e38b90550e51f6f307ec7bd7a72
Keyplexex <shc.dasha@gmail.com>
2022-09-28 01:10:47 +0300

    Edits

--- model.py
+++ model.py
@@ -1,6 +1,11 @@
+import random
+
+
 class Model:
     # Заполнить список слов
-    words = []
+    words = ['яблоко', 'апельсин', 'картошка', 'тотошка']
 
-    def get_random_word() -> str:
+    def get_random_word(self, words) -> str:
         """Сгенерировать рандомное слово из массива self.words"""
+        random_word = random.choice(words)
+        return random_word
