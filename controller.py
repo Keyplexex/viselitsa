65e48c221af33116c641b392c7385f6bde74d587
Keyplexex <shc.dasha@gmail.com>
2022-09-28 01:10:47 +0300

    .

--- controller.py
+++ controller.py
@@ -2,19 +2,29 @@ class Controller:
     def __init__(self, word):
         """
         Функция начала игры. Вся логика этой функции заключается в инициализации пространства имен
-        (
-            self.word = word
-            self.guessed_word_indexes = []
-        )
+        ()
         (https://docs.python.org/3/tutorial/classes.html)
         """
-    
-    def check_letter(self, letter):
+        self.word = word
+        self.guessed_word_indexes = []
+
+    def check_letter(self, letter, word, guessed_letters_indexes):
         """
         Перенести логику из main.py
         """
-    
-    def check_end_game(self):
+        indexes = []
+        for index, iter_letter in enumerate(word):
+            if iter_letter == letter:
+                indexes.append(index)
+        guessed_letters_indexes.append(indexes)
+        return indexes
+
+    def check_end_game(self, word, guessed_letters_indexes):
         """
         Перенести логику из main.py
         """
+        end_game = True
+        for i in range(len(word)):
+            if i not in guessed_letters_indexes:
+                end_game = False
+        return end_game
--- view.py
+++ view.py
@@ -5,7 +5,16 @@ class View:
         """
         Содержимое функции взять из main.py
         """
+
+        letter = str(input('Введите букву: '))
+        return letter
+
     def display_word(guessed_letters_indexes: List[int], word: str) -> None:
         """
         Содержимое функции взять из main.py
         """
+        displayword = list(word)
+        for i in range(len(word)):
+            if i not in guessed_letters_indexes:
+                displayword[i] = "_"
+        print(*displayword, sep='')
